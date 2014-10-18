80cae2815668b61094c012c9082f98f10d55bfa4
Gaspard <gaspard.benoit@student.ecp.fr>
2014-10-18 12:42:43 +0200

    Keep Alive Log

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ var log = console.log.bind(console);
 
 var interval = setInterval(function(){
 	log('Alive');
-})
+}, 2000)
 
 var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', "*");
